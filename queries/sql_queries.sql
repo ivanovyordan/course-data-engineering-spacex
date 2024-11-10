ea3c95f5b531e9d77ddc9ce073b0b0c672bc8de2
Yordan Ivanov <mail@ivanovyordan.com>
2024-11-10 08:33:55 +0200

    Extract Insights from Your Data
    Add intermediate data analysis queries

--- queries/sql_queries.sql
+++ queries/sql_queries.sql
@@ -7,3 +7,26 @@ where success = true;
 select *
 from dbt_spacex.launches
 order by launch_date desc;
+
+select
+    rocket,
+    count(*) as launch_count
+from dbt_spacex.launches
+group by rocket;
+
+select
+    rocket,
+    avg(
+        case
+            when success = true then 1
+            else 0
+        end
+    ) as success_rate
+from dbt_spacex.launches
+group by rocket;
+
+select
+    year(launch_date) as launch_year,
+    count(*) as launches_number
+from dbt_spacex.launches
+group by launch_year;
