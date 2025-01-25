ca71c9f5c7aaff32fd8f4b7a0fa390680de9d75b
Yordan Ivanov <mail@ivanovyordan.com>
2025-01-25 12:55:48 +0200

    Improve data manipulation output

--- src/data_manipulation.py
+++ src/data_manipulation.py
@@ -7,8 +7,19 @@ data = {
 
 df = pd.DataFrame(data)
 
+print("All data")
 print(df)
+
+print("")
+print("Rockets")
 print(df["rocket"])
 
 falcon9_df = df[df["rocket"] == "Falcon 9"]
+print("")
+print("Falcon 9")
+print(falcon9_df)
+
 df["success_rate"] = [0.4, 0.98, 1.0]
+print("")
+print("New success rates")
+print(df["success_rate"])
